bae5cf93bcecdb3607d2a9dfdbfe2a6042dd87b2
Андрей <lau47@mail.ru>
2025-06-25 09:55:07 +0300

    UsersController

--- WebApplicationMVC/Controllers/HomeController.cs
+++ WebApplicationMVC/Controllers/HomeController.cs
@@ -51,10 +51,5 @@ namespace WebApplicationMVC.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public async Task<IActionResult> Authors()
-        {
-            var authors = await _repo.GetUsers();
-            return View(authors);
-        }
     }
 }
---
+++ WebApplicationMVC/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationMVC.Repositories;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        private readonly IBlogRepository _repo;
+
+        // Также добавим инициализацию в конструктор
+        public UsersController(IBlogRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var authors = await _repo.GetUsers();
+            return View(authors);
+        }
+    }
+}
